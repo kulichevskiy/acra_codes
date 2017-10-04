924d132caaaadee10ad5fe7a014c00815a59d3b0
Alexey Kulichevskiy <alexey.kulichevskiy@gmail.com>
2017-10-04 22:58:53 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import jsonify
+from operator import itemgetter
+import pandas as pd
+import spacy
+
+codes = pd.read_csv('acra_codes.csv')
+app = Flask(__name__)
+
+nlp = spacy.load('en')
+
+@app.route('/<string:term>')
+def hello_world(term, codes=codes):
+    distances = []
+
+    for index, row in codes.iterrows():
+        entry = {}
+        entry['code'] = row.code
+        entry['title'] = row.title
+        entry['similarity'] = nlp(row.title).similarity(nlp(term))
+        distances.append(entry)
+
+    sorted_distances = sorted(distances, key=itemgetter('distance'), reverse=True)
+    return jsonify(sorted_distances[0:19])
